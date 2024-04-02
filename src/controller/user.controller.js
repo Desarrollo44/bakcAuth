3f2a5431a01f65b000122e652b4ed198fd9adf0a
Desarrollo44 <david.avendano@ingeal.com>
2024-04-02 10:43:57 -0500

    corregir

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,10 +1,9 @@
-const { use } = require("../app/app");
 
 function postUser(req,res){
     try {
         const{username,password}=req.body;
         console.log('Captured obj: nombre: ', username,' contraseña: ',password);
-        res.status(200).send('Obj captured successfully!, user',username,' se ha registrado');
+        res.status(200).send('Obj captured successfully!');
     } catch (error) {
         res.status(500).json({message:'Error interno del servidor'});
     }
